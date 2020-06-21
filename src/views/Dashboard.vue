325ede63c461794365980c4cd920b477de64ce48
collins nyamao <collins.nyamao@strathmore.edu>
2020-06-21 15:46:25 +0300

    fix: ip fix

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -248,7 +248,7 @@ import SocialTrafficTable from './Dashboard/SocialTrafficTable';
 import PageVisitsTable from './Dashboard/PageVisitsTable';
 import axios from 'axios';
 
-let address = 'https://127.0.0.1:3002';
+let address = 'https://10.30.20.89:3002';
 export default {
   components: {
     LineChart,
